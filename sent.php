3f7e6b8af3ea3cf1fa97a1430b309e15d4fb0c0b
spachill <87156536+spachill@users.noreply.github.com>
2021-07-17 21:37:14 +0900

    Create sent.php

---
+++ sent.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="stylesheet.css">
+    <meta charset="utf-8">
+  </head>
+  <body>
+    
+<?php
+    <form action="sent.php" method ="post">
+     名前を入力してください
+     <input type="text" name="name">
+     </form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+  </body>
+</html>
+    
